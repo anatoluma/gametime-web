20ecd5debc2c1abb3805d62bc8ada0b7e5cf75ed
Anatol Umanet <anatoluma@gmail.com>
2026-03-20 10:06:35 +0200

    force rebuild

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
+// Anon client for browser/public operations
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
